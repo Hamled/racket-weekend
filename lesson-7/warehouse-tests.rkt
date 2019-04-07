111d8e4d249fe0626012c7de3e13023752e70be1
Charles Ellis <hamled@hamled.name>
2019-04-07 06:50:46 -0700

    Lesson 7

---
+++ lesson-7/warehouse-tests.rkt
@@ -0,0 +1,25 @@
+#lang racket
+(require (file "../lesson-3/warehouse.rkt"))
+
+(module+ test
+  (require rackunit))
+
+(module+ test
+  (check-true (location? super-shoes-loc))
+
+  (check-= 5
+           (location-aisle super-shoes-loc)
+           0))
+
+(module+ test
+  (define b (bin (location "A" 5 1)
+                 "Fancy Pants"
+                 1
+                 #t))
+  (define picked-b (pick b))
+
+  (check-= 1 (bin-qty b) 0)
+  (check-= 0 (bin-qty picked-b) 0)
+  (check-exn exn:fail?
+             (lambda ()
+               (pick picked-b))))
